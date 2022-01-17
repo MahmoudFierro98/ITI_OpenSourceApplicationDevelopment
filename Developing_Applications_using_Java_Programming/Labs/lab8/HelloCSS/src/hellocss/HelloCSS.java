ac1d58f57cc44f9ea5f637beb951870183dcfd0d
Mahmoud Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-01-17 14:45:21 +0200

    Add files via upload

--- Developing_Applications_using_Java_Programming/Labs/lab8/HelloCSS/src/hellocss/HelloCSS.java
+++ Developing_Applications_using_Java_Programming/Labs/lab8/HelloCSS/src/hellocss/HelloCSS.java
@@ -49,7 +49,7 @@ public class HelloCSS extends Application
         Rectangle rectangle = new Rectangle(0, 0, 400, 300);
         rectangle.setId("bg");
         /* Create Root branch */
-	    StackPane root = new StackPane();
+		StackPane root = new StackPane();
         root.getChildren().add(rectangle);
         root.getChildren().add(helloWorld);
         root.getChildren().add(reflectedHelloWorld);
@@ -58,13 +58,13 @@ public class HelloCSS extends Application
         /* Load CSS Styles */
         scene.getStylesheets().add(getClass().getResource("Styles.css").toString());
         /* Set title */
-	    primaryStage.setTitle("Hello World");
-	    /* Add scene to stage */
-	    primaryStage.setScene(scene);
+		primaryStage.setTitle("Hello World");
+		/* Add scene to stage */
+		primaryStage.setScene(scene);
         /* Change Layout */
         primaryStage.initStyle(StageStyle.DECORATED);
         /* Start */
-	    primaryStage.show();
+        primaryStage.show();
     }
     public static void main(String args[])
     {
