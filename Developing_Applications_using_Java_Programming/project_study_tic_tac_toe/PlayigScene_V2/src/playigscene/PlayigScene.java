5412792a6b00f1b4abfa5c4f4865856c37197620
Mahmoud Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-01-28 02:58:12 +0200

    UPDATE PlayingScene - V2

---
+++ Developing_Applications_using_Java_Programming/project_study_tic_tac_toe/PlayigScene_V2/src/playigscene/PlayigScene.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package playigscene;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mahmoud kamal
+ */
+public class PlayigScene extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("PlayingScene.fxml"));
+        Parent root = new PlayingSceneBase("vsComputer", "Fierro_1998", "Computer");
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
